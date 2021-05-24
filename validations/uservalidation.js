c680ece8b2c639215c53c07d63eea48b753f4119
Senagoka <gokasena24@gmail.com>
2021-05-24 13:18:36 +0000

    change validation

--- validations/uservalidation.js
+++ validations/uservalidation.js
@@ -3,8 +3,8 @@ const Joi = require("joi");
 // register user validation
 const validateAdduser = new Joi.object({
   name: Joi.string().min(10).required().max(20),
-  email: Joi.string().min(12).required().max(40).email(),
-  phone: Joi.string().min(12).required().max(40),
+  email: Joi.string().email().min(12).required().max(1040),
+  phone: Joi.string().min(10).required().max(40),
   password: Joi.string().min(8).max(50).required(),
 });
 
